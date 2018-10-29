432206532bf0eb2a073cfee3753bb1ce702b631c
Trystan Larey-Williams <trystan@bluecanvas-studios.com>
2018-10-28 17:50:21 -0700

    test render of tycho2 working

---
+++ Engine/RenderProgram.cpp
@@ -0,0 +1,8 @@
+//
+//  RenderProgram.cpp
+//  Infinitarium
+//
+//  Created by Trystan Larey-Williams on 10/21/18.
+//
+
+#include "RenderProgram.hpp"
---
+++ Engine/RenderProgram.hpp
@@ -0,0 +1,13 @@
+//
+//  RenderProgram.hpp
+//  Infinitarium
+//
+//  Created by Trystan Larey-Williams on 10/21/18.
+//
+
+#ifndef RenderProgram_hpp
+#define RenderProgram_hpp
+
+#include <stdio.h>
+
+#endif /* RenderProgram_hpp */
