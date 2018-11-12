abdcd4430802245da98c6fd57415d9abad387b7d
Trystan Larey-Williams <trystan@bluecanvas-studios.com>
2018-11-12 07:49:10 -0800

    added missing file(s)

---
+++ main.cpp
@@ -0,0 +1,26 @@
+///
+///
+///
+
+#include "Engine/PointCloud.hpp"
+#include "Engine/Scene.hpp"
+#include "Engine/ApplicationWindow.hpp"
+#include "Engine/Application.hpp"
+#include "Engine/Simulation.hpp"
+#include "Engine/MotionControllerOrbit.hpp"
+
+int main(int argc, const char * argv[]) {
+   std::shared_ptr<IApplication> app = IApplication::Create();
+   std::shared_ptr<IRenderContext> context = IRenderContext::Create( 100, 100, 1920, 1080 );
+   std::shared_ptr<IApplicationWindow> window = IApplicationWindow::Create( *context );
+   
+   std::shared_ptr<Scene> s = std::make_shared<Scene>();
+   s->setRenderContext( context );
+   s->load("/Users/trystan/tyco2.ieb");
+   std::shared_ptr<IMotionController> ctrl{ std::make_shared<Orbit>() };
+   s->setMotionController( ctrl );
+   
+   Simulation simulation;
+   simulation.setScene( s );
+   app->run();
+}
