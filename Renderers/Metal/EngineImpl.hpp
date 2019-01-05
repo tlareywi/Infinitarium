0b819381f0a84d6e61d3a7bae10865c719338f50
Trystan Larey-Williams <trystan@bluecanvas-studios.com>
2019-01-05 11:43:12 -0800

    Revert "removed deprecated files"
    This reverts commit bc28d4fe8302e5b0bc30bf9eced1cfc337eae1aa.

---
+++ Renderers/Metal/EngineImpl.hpp
@@ -0,0 +1,13 @@
+//
+//  DataBuffer.hpp
+//  Infinitarium
+//
+//  Created by Trystan Larey-Williams on 10/13/18.
+//
+
+#pragma once
+
+
+
+
+
---
+++ Renderers/Metal/EngineImpl.mm
@@ -0,0 +1,31 @@
+//
+//  DataBuffer.cpp
+//  Infinitarium
+//
+//  Created by Trystan Larey-Williams on 10/13/18.
+//
+
+#include "EngineImpl.hpp"
+#include "ShaderTypes.h"
+#include "UniformMap.h"
+
+#import <Metal/Metal.h>
+#import <Cocoa/Cocoa.h>
+#import <QuartzCore/CAMetalLayer.h>
+
+#include "../../Engine/RenderCommand.hpp"
+#include "../../Engine/RenderProgram.hpp"
+#include "../../Engine/RenderContext.hpp"
+
+#include <sstream>
+#include <fstream>
+
+#include "../../config.h"
+
+
+
+
+
+
+
+
